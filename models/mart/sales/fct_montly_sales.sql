4a127898b5b76c74c5aa9770e782bbb74a1bb008
Pablo Damm <pablodamm@gmail.com>
2026-07-29 13:36:35 +0000

    -

---
+++ models/mart/sales/fct_montly_sales.sql
@@ -0,0 +1,13 @@
+{{ config(
+    tags=['dash_sales_bi']
+) }}
+
+SELECT
+    DDATE,
+    ID_SALESPERSON,
+    ID_CARMAKE,
+    TOTAL_SALE,
+    TOTAL_COM
+FROM {{ ref("psg_montly_sales_make") }} AS psgm
+LEFT JOIN {{ ref("dim_carmake") }} AS dimcm ON psgm.CARMAKE = dimcm.CARMAKE
+LEFT JOIN {{ ref("dim_salesperson") }} AS dimsp ON psgm.SALESPERSON = dimcm.SALESPERSON
